76ff0bc817b1af25c943b5d8d1790bdb4cb81899
Wiggidy <rlr0017@auburn.edu>
2026-01-28 15:33:18 -0600

    attempt to fix JS values being Map instead of Record

--- objectiveai-rs-wasm-js/src/lib.rs
+++ objectiveai-rs-wasm-js/src/lib.rs
@@ -53,6 +53,7 @@ pub fn validateEnsembleLlm(llm: JsValue) -> Result<JsValue, JsValue> {
         .try_into()
         .map_err(|e: String| JsValue::from_str(&e))?;
     // serialize
+    let llm: serde_json::Value = serde_json::to_value(llm).unwrap();
     let llm: JsValue = serde_wasm_bindgen::to_value(&llm)?;
     Ok(llm)
 }
@@ -85,6 +86,7 @@ pub fn validateEnsemble(ensemble: JsValue) -> Result<JsValue, JsValue> {
         .try_into()
         .map_err(|e: String| JsValue::from_str(&e))?;
     // serialize
+    let ensemble: serde_json::Value = serde_json::to_value(ensemble).unwrap();
     let ensemble: JsValue = serde_wasm_bindgen::to_value(&ensemble)?;
     Ok(ensemble)
 }
